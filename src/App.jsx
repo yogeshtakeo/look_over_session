139890d3d9d5107b8a8bf71527bf886c1f8d4224
uniquemozilla22 <uniquegaming26@gmail.com>
2023-07-10 15:08:00 -0400

    Friday class commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,35 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Heading from "./heading";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const [username, setUserName] = useState();
+  let hello = 100;
+
+  const handleClickOnCout = () => {
+    setCount(count + 1);
+    hello = 10000000;
+
+    console.log(hello);
+  };
+
+  // if (count < 5 || count === 5) {
+  //   return <Heading title={count} />;
+  // }
+  // else{
+  //   return count
+  // }
+
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div onClick={handleClickOnCout}>
+      {count || 5}
+      <br />
+      {username || "Guest"}
+      {hello}
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const div = document.createElement("div");
+div.setAttribute("id", "root");
+
+div.innerHTML = "<h1>Hello</h1>";
+
+const body = document.body;
+
+body.appendChild(div);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
